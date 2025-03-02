c686ee5ec3f286523cb65585dbe650fbc5b68e73
i-am-harveyt <haotingtong30@gmail.com>
2025-03-02 16:32:07 +0800

    Fix: import problem in cors; Add: example endpoints in app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import v1 from "./routes/v1/api.js";
 import bodyParser from "body-parser";
 import cors from "./lib/cors.js";
+import userRouter from "./routes/v1/user.js";
 
 /**
  * @param {express.Response} res - We can manage Response with this arg.
@@ -15,6 +16,12 @@ async function pong(_, res) {
 const app = express();
 app.use("/", cors);
 app.use(bodyParser.json());
+
+/* for example */
+app.get("/", (_, res) => res.send("<h1>GET!</h1>"));
+app.post("/", (_, res) => res.send("<h1>POST!</h1>"));
+app.use("/user", userRouter);
+
 app.get("/ping", pong);
 app.use("/api/v1", v1);
 app.listen(3000);
